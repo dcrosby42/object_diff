579d41b683e9a33988c7c85d82c23a568932fb1b
David Crosby <dcrosby42@gmail.com>
2014-07-10 08:38:44 -0400

    Scalars and partial testing of Hash, todo: Array

--- lib/rdiff.rb
+++ lib/rdiff.rb
@@ -1,108 +1,124 @@
 require 'set'
+module Rdiff
+  class Diff
+    attr_reader :a, :b
+    def initialize(a,b)
+      @a = a
+      @b = b
+    end
+    def changes
+      []
+    end
 
-class Diff
-  attr_reader :a, :b
-  def initialize(a,b)
-    @a = a
-    @b = b
+    def ==(o)
+      o and o.class == self.class and a == o.a and b == o.b
+    end
+    alias_method :eq, :==
+
+    def to_s
+      inspect
+    end
+    def inspect
+      "<#{a.inspect}->#{b.inspect}>"
+    end
   end
-  def changes
-    []
+
+  class HashDiff < Diff
+    def initialize(a,b,diffs,added,lost)
+      super a,b
+      @diffs = diffs
+      @added = added
+      @lost = lost
+      @changes = []
+    end
+    def changes
+      @changes
+    end
+
+    def inspect
+      @diffs.inspect
+    end
+  end
+
+  class ArrayDiff < Diff
+    def initialize(a,b,adds,drops)
+      super a,b
+      @adds = adds
+      @drops = drops
+      @changes = []
+    end
+    def changes
+      @changes
+    end
+  end
+
+  class << self
+
+    def diff(a,b)
+      if Hash === a and Hash === b
+        diff_hash(a, b)
+      elsif Array === a and Array === b
+        diff_array(a,b)
+      else
+        diff_scalar_value(a,b)
+      end
+    end
+
+    private
+
+    def make_diff(a,b)
+      Diff.new(a,b)
+    end
+
+    def make_hash_diff(a,b,diffs,adds,drops)
+      HashDiff.new(a,b,diffs,adds,drops)
+    end
+
+    def make_array_diff(a,b,adds,drops)
+      ArrayDiff.new(a,b,adds,drops)
+    end
+
+    def diff_scalar_value(a,b)
+      if a != b
+        make_diff a, b
+      else
+        nil
+      end
+    end
+
+    def diff_hash(a,b)
+      a_keys = a.keys
+      b_keys = b.keys
+      new_keys = b_keys - a_keys
+      missing_keys = a_keys - b_keys
+      shared_keys = (Set.new(a_keys) & Set.new(b_keys)).to_a.sort
+      # all_keys = Set.new(a_keys + b_keys).to_a.sort
+
+      adds = new_keys.map do |key| b[key] end
+      drops = missing_keys.map do |key| a[key] end
+      diffs = {}
+      shared_keys.each do |key| 
+        d = diff(a[key], b[key]) 
+        if d
+          diffs[key] = d
+        end
+      end
+
+      if adds.empty? and drops.empty? and diffs.keys.empty?
+        nil
+      else
+        make_hash_diff a,b, diffs, adds, drops
+      end
+    end
+
+    def diff_array(a,b)
+      new_items = b - a
+      missing_items = a - b
+      if new_items.empty? and missing_items.empty?  
+        nil
+      else
+        make_array_diff a,b, new_items, missing_items
+      end
+    end
   end
 end
-
-class HashDiff < Diff
-  def initialize(a,b,diffs,added,lost)
-    super a,b
-    @diffs = diffs
-    @added = added
-    @lost = lost
-    @changes = []
-  end
-  def changes
-    @changes
-  end
-end
-
-class ArrayDiff < Diff
-  def initialize(a,b,adds,drops)
-    super a,b
-    @adds = adds
-    @drops = drops
-    @changes = []
-  end
-  def changes
-    @changes
-  end
-end
-
-def make_diff(a,b)
-  Diff.new(a,b)
-end
-
-def make_hash_diff(a,b,diffs,adds,drops)
-  HashDiff.new(a,b,diffs,adds,drops)
-end
-
-def make_array_diff(a,b,adds,drops)
-  ArrayDiff.new(a,b,adds,drops)
-end
-
-def diff_scalar_value(a,b)
-  if a != b
-    make_scalar_diff a, b
-  else
-    nil
-  end
-end
-
-def diff_hash(a,b)
-  a_keys = a.keys
-  b_keys = b.keys
-  new_keys = b_keys - a_keys
-  missing_keys = a_keys - b_keys
-  shared_keys = (Set.new(a_keys) & Set.new(b_keys)).to_a.sort
-  # all_keys = Set.new(a_keys + b_keys).to_a.sort
-
-  adds = new_keys.map do |key| b[key] end
-  drops = missing_keys.map do |key| a[key] end
-  diffs = shared_keys.map do |key| 
-    diff(a[key], b[key]) 
-  end.reject do |d| 
-    d.nil? 
-  end
-
-  if adds.empty? and drops.empty? and diffs.empty?
-    nil
-  else
-    make_hash_diff a,b, diffs, adds, drops
-  end
-end
-
-def diff_array(a,b)
-  new_items = b - a
-  missing_items = a - b
-  if new_items.empty? and missing_items.empty?  
-    nil
-  else
-    make_array_diff a,b, new_items, missing_items
-  end
-end
-
-def diff(a,b)
-  if Hash === a and Hash === b
-    diff_hash(a, b)
-  elsif Array === a and Array === b
-    diff_array(a,b)
-  else
-    d = diff_scalar_value(a,b)
-  end
-end
-
-__END__
-
-DIFF:
-  when a and b are Hash
-  when a and b are Array
-  else
-    
--- spec/lib/rdiff_spec.rb
+++ spec/lib/rdiff_spec.rb
@@ -1,5 +1,57 @@
-describe "hi" do
-  it "works" do
-    expect(1).to eq 2
+require 'spec_helper'
+
+describe "rdiff" do
+  def diff(a,b)
+    Rdiff.diff(a,b)
+  end
+  def Diff(a,b)
+    Rdiff::Diff.new(a,b)
+  end
+
+  describe "base case:" do
+    it "nils are same" do
+      expect(diff(nil,nil)).to be nil
+    end
+
+    it "0 and 0 are same" do
+      expect(diff(0,0)).to be nil
+    end
+
+    it "'a' and 'a' are same" do
+      expect(diff("a","a")).to be nil
+    end
+
+    it "{} and {} are same" do
+      expect(diff({},{})).to be nil
+    end
+    it "[] and [] are same" do
+      expect(diff({},{})).to be nil
+    end
+  end
+
+  describe "simple scalars:" do
+    it "1 and 2 are different" do
+      d = diff(1,2)
+      expect(d).to eq Diff(1,2)
+    end
+    it "nil and 2 are different" do
+      d = diff(nil,2)
+      expect(d).to eq Diff(nil,2)
+    end
+    it "'a' and 'b' are different" do
+      d = diff('a','b')
+      expect(d).to eq Diff('a','b')
+    end
+  end
+
+  describe "Hash:" do
+    it "finds changed keys" do
+      d = diff({a:"A1"},{a:"A2"})
+      p d
+      expect(d).to eq nil
+    end
+  end
+
+  describe "Array:" do
   end
 end
